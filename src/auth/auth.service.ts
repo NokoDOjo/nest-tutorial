6fa7aa45b16871db3049e190399618d092759a1a
Benny <happybenny0424@gmail.com>
2022-04-07 09:27:53 +0000

    add signin check email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,26 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'I am signed in'
+  async signin(dto: AuthDto) {
+    // find the user by email
+    // if user does not exist throw exception
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email
+        },
+      })
+    console.log(dto)
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect')
+    }
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password
+    )
+    // if password incorrect throw exception
+    delete user.hash
+    return user
   }
 }
